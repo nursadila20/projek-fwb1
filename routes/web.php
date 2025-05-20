289a58f1d67860bd777fa0fa2cd9a2162ada91a2
nursadila20 <nursadila951@gmail.com>
2025-05-20 21:42:47 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Dashboard;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/beranda', [Dashboard::class, 'index'])->name('beranda');
